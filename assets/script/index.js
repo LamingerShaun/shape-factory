0ddf872f41ea13d9d676e2abcde90ece8a133c65
Shaun <117479605+LamingerShaun@users.noreply.github.com>
2022-11-27 14:50:06 -0600

    index fix

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,3 +1,8 @@
 'use strict';
 
 import { select, selectAll, print } from "./utils";
+
+function createShape() {
+    let div = document.createElement('div');
+    document.body.appendChild(div);
+}
--- assets/script/utils.js
+++ assets/script/utils.js
@@ -1,1 +1,13 @@
 'use strict';
+
+function select(selector, parent = document) {
+    return parent.querySelector(selector);
+}
+
+function selectAll(selector, parent = document) {
+    return [...parent.querySelectorAll(selector)];
+}
+
+
+
+export { select, selectAll, print };
